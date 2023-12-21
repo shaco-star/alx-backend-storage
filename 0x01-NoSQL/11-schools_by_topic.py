781d8ee29f764ab200aa8515e0bafef130204b3e
shaco-star <sherifm846@gmail.com>
2023-12-21 04:19:50 -0800

    fixed

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,4 @@
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
     query = {"topics": {"$elemMatch": {"$eq": topic}}}
-    return [doc for doc in monogo_collection.find(query)]
+    return [doc for doc in mongo_collection.find(query)]
